4d9649f32a66251190716040a35fef29b34bc519
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-10-06 02:43:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
-document.addEventListener("DOMContentLoaded", init, false);
+const storageInput = document.querySelector('#stud');
+const button = document.querySelector('#submitBtn');
+const storedInput = localStorage.getItem('textinput')
 
-let stud;
 
-function init() {
-    stud = document.querySelector("#stud");
-
-    let elem = Array.from(document.querySelectorAll("#form_stud textarea"));
-    elem.forEach(e => e.addEventListener("input", handleChange, false));
-
-    let cached = getForm();
-    if (cached) {
-        console.log("cached retrieve");
-        stud.value = cached.stud;
-    }
-
-    document.querySelector('#mainForm').addEventListener('submit', () => {
-    window.localStorage.removeItem('form');
-  }, false);
+if (storageInput) {
+    storageInput.value = storedInput
 }
 
-function handleChange(e) {
-    console.log('handleChange');
-    let form = {};
-    form.stud = stud.value;
-    saveForm(form);
+const saveToLocalStorage = (e) => {
+    e.preventDefault();
+    localStorage.setItem('textinput', storageInput.value)
 }
 
-function saveForm(form) {
-    let f = JSON.stringify(form);
-    window.localStorage.setItem('stud', f);
-}
-
-function getForm() {
-    let f = window.localStorage.getItem('stud');
-    if (f) return JSON.parse(f);
-}
+button.addEventListener('click', saveToLocalStorage)
